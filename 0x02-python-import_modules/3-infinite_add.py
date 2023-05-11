ffc04918202e122192f8e89a43a6520e4bb6c31f
ManNjoro <elijohnmwoho@gmail.com>
2023-05-11 04:49:51 -0700

    using imported modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import sys
-
-if len(sys.argv) == 1:
-    print("0")
-else:
-    result = 0
-    for arg in sys.argv[1:]:
-        result += int(arg)
-    print(result)
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("0")
+    else:
+        result = 0
+        for arg in sys.argv[1:]:
+            result += int(arg)
+        print(result)
