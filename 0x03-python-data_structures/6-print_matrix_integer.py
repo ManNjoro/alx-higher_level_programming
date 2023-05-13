c1d424e3faa3134af9fc1de7d6e034ce58354b43
ManNjoro <elijohnmwoho@gmail.com>
2023-05-13 04:21:00 -0700

    corrections

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".formart(matrix[i][j]), end=' ')
+    if not matrix:
+        return
+    for row in matrix:
+        for column in row:
+            print("{0:3d}".format(column), end=" ")
         print()
