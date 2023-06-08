bdfac913565cf6a56a98ef4888e1552d9539df02
ManNjoro <elijohnmwoho@gmail.com>
2023-06-08 16:40:53 -0700

    corrections

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
+"""Module to find the max integer in a list
 """
-Function to find and return the maximum integer in a list.
-"""
 
 
-def max_integer(lst=[]):
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        returns None
     """
-    Args:
-        lst (list): List of integers.
-
-    Returns:
-        int: Maximum integer in the list. If the list is empty, returns None.
-    """
-    if not isinstance(lst, list):
-        raise TypeError("Input must be a list")
-    if len(lst) == 0:
+    if len(list) == 0:
         return None
-    result = lst[0]
-    for num in lst:
-        if num > result:
-            result = num
-    return result
+    res = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > res:
+            res = list[i]
+        i += 1
+    return res
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+"""Unittest for max_integer([..])
 """
-Unittest for max_integer([..])
-"""
-
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
@@ -15,62 +12,60 @@ class TestMaxInteger(unittest.TestCase):
     def test_func_doc(self):
         self.assertTrue(len(max_integer.__doc__) > 1)
 
-    def test_regular_list(self):
-        """Test with a regular list"""
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    def test_emptyargs(self):
+        self.assertIsNone(max_integer())
 
-    def test_reversed_list(self):
-        """Test with a reversed list"""
-        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+    def test_empty(self):
+        self.assertIsNone(max_integer([]), None)
+        self.assertIsNone(max_integer([None]), None)
+        with self.assertRaises(TypeError):
+            max_integer(None)
+        with self.assertRaises(TypeError):
+            max_integer([2, 4, 5, 6, None])
 
-    def test_empty_list(self):
-        """Test with an empty list"""
-        self.assertIsNone(max_integer([]))
+    def test_one(self):
+        self.assertEqual(max_integer([55]), 55)
 
-    def test_single_element_list(self):
-        """Test with a list containing a single element"""
-        self.assertEqual(max_integer([7]), 7)
+    def test_integers(self):
+        self.assertEqual(max_integer([1, 5, 6, 4]), 6)
+        self.assertEqual(max_integer([55, 55, 55, 55]), 55)
+        self.assertEqual(max_integer([6, 2, 3, 4]), 6)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+        self.assertEqual(max_integer([3]), 3)
 
-    def test_negative_numbers(self):
-        """Test with a list containing negative numbers"""
-        self.assertEqual(max_integer([-1, -5, -3, -2]), -1)
+    def test_integer_neg(self):
+        self.assertEqual(max_integer([-44, -66, -43, -22]), -22)
+        self.assertEqual(max_integer([1, 3, 74, -2]), 74)
 
-    def test_duplicate_numbers(self):
-        """Test with a list containing duplicate numbers"""
-        self.assertEqual(max_integer([1, 2, 3, 2, 1]), 3)
+    def test_float(self):
+        self.assertEqual(max_integer([2.2, 3.3, 4.4, 5.5]), 5.5)
+        self.assertEqual(max_integer([-44.5, -53.22, -2.224, -3.14]), -2.224)
 
-    def test_positive_numbers(self):
-        result = max_integer([1, 2, 3, 4, 5])
-        self.assertEqual(result, 5)
+    def test_string(self):
+        self.assertEqual(max_integer("12345323121"), "5")
+        self.assertEqual(max_integer("1, 2, 3"), "3")
 
-    def test_mixed_numbers(self):
-        result = max_integer([-10, 5, -3, 0, 7, -2])
-        self.assertEqual(result, 7)
+    def test_alpha(self):
+        self.assertEqual(max_integer(["xyz"]), "xyz")
+        self.assertEqual(max_integer(["abc", "xyz"]), "xyz")
+        self.assertEqual(max_integer(["abc", "1", "4", "xyz"]), "xyz")
 
-    def test_large_list(self):
-        lst = list(range(1, 10001))
-        result = max_integer(lst)
-        self.assertEqual(result, 10000)
+    def test_other_type(self):
+        self.assertEqual(max_integer([[1, 2, 3, 4], [1, 4, 3, 2]]), [1, 4, 3, 2])
+        self.assertEqual(max_integer((3, 4, 5, 6, 19)), 19)
+        with self.assertRaises(KeyError):
+            max_integer({1: 1, 4: 5, 5: 4})
+
+    def test_mix(self):
+        with self.assertRaises(TypeError):
+            max_integer(["help", 22, "44", 4.5, -3])
+        with self.assertRaises(TypeError):
+            max_integer(["help", [22], "44", 5.5, -55])
 
     def test_none(self):
         with self.assertRaises(TypeError):
             max_integer(None)
 
-    def test_max_integer_with_strings(self):
-        self.assertEqual(max_integer(["apple", "banana", "cherry"]), "cherry")
-        self.assertEqual(max_integer(["cat", "dog", "elephant"]), "elephant")
-        self.assertEqual(max_integer(["Hello", "World"]), "World")
-        self.assertEqual(max_integer(["a", "b", "c"]), "c")
-        self.assertEqual(max_integer(["abc", "def", "ghi"]), "ghi")
-
-    def test_max_integer_with_non_list_input(self):
-        with self.assertRaises(TypeError):
-            max_integer(123)
-        with self.assertRaises(TypeError):
-            max_integer("hello")
-        with self.assertRaises(TypeError):
-            max_integer(("a", "b", "c"))
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
