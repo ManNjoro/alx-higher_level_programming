e88c96d997300a8e20e865e0e7f7b453c12a5b08
ManNjoro <elijohnmwoho@gmail.com>
2023-09-01 07:07:41 -0700

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,8 +11,7 @@ if __name__ == "__main__":
     q = argv[1] if len(argv) > 1 else ""
     data = {'q': q}
     try:
-        r = requests.post('http://0.0.0.0:5000/search_user',
-                           data).json()
+        r = requests.post('http://0.0.0.0:5000/search_user', data).json()
         if 'id' in r and 'name' in r:
             print("[{}] {}".format(r.get('id'), r.get('name')))
         else:
