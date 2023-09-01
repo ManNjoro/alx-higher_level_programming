f8aec5419f5264f01ae4808e6efaf097bb374ccd
ManNjoro <elijohnmwoho@gmail.com>
2023-09-01 06:39:46 -0700

    post an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,4 +12,4 @@ from sys import argv
 if __name__ == "__main__":
     data = {'email': argv[2]}
     r = requests.post(argv[1], data)
-    print("Your email is: {}".format(r.text))
+    print(r.text)
